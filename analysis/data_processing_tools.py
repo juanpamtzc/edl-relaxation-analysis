978600923e770675f8d70fab4a6af98a3430188d
JPMC <jp.martinez.cordeiro@utexas.edu>
2025-12-09 23:26:58 -0600

    added data_processing_tools.py script to house any functions needed - apart from reading functions - to process the data

---
+++ analysis/data_processing_tools.py
@@ -0,0 +1,11 @@
+from analysis.reading_tools import readTRJFile, readDatFile, readTRJFile_stresses, readThermodynamicData
+
+# This function processes corresponding data, trajectory, velocity, force, stress, and thermodynamic output files and returns their contents for analysis
+def process_filename(trj_filename, vel_filename, force_filename, stress_filename, dat_filename, thermo_filename):
+    trj, local2global, global2local=readTRJFile(trj_filename, True, None)
+    vel=readTRJFile(vel_filename, True, None)[0]
+    force=readTRJFile(force_filename, True, None)[0]
+    stress=readTRJFile_stresses(stress_filename, True, None)[0]
+    data=readDatFile(dat_filename)
+    thermodynamic_data=readThermodynamicData(thermo_filename)
+    return trj, global2local, local2global, vel, force, stress, data, thermodynamic_data
