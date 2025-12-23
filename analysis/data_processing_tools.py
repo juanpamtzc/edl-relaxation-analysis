d79e9d4ad06be243dbe0e186df84f3aff11ae20f
JPMC <jp.martinez.cordeiro@utexas.edu>
2025-12-23 17:01:05 -0600

    Added function headers and type hinting

--- analysis/data_processing_tools.py
+++ analysis/data_processing_tools.py
@@ -1,7 +1,30 @@
 from analysis.reading_tools import readTRJFile, readDatFile, readTRJFile_stresses, readLAMMPSThermodynamicFile
+from typing import Optional
 
-# This function processes corresponding data, trajectory, velocity, force, stress, and thermodynamic output files and returns their contents for analysis
-def process_filename(trj_filename, vel_filename, force_filename, stress_filename, dat_filename, thermo_filename, thermo_process_ID=1):
+def process_filename(trj_filename: str, vel_filename: str, force_filename: str, stress_filename: str, dat_filename: str, thermo_filename: str, thermo_process_ID: Optional[int] = 1) -> tuple:
+    """
+    Processes the provided filenames for trajectory, velocity, force, stress, data, and thermodynamic output files.
+
+    Args:
+        trj_filename:           The filename for the trajectory data.
+        vel_filename:           The filename for the velocity data.
+        force_filename:         The filename for the force data.
+        stress_filename:        The filename for the stress data.
+        dat_filename:           The filename for the data file. 
+        thermo_filename:        The filename for the thermodynamic data.
+        thermo_process_ID (Optional[int]): The process ID for thermodynamic data processing. Default is 1.
+    
+    Returns:
+        trj:                    A numpy array shaped (M, N, 3) containing trajectory data.
+        global2local:           A dictionary mapping global atom IDs to local atom IDs.
+        local2global:           A dictionary mapping local atom IDs to global atom IDs.
+        vel:                    A numpy array shaped (M, N, 3) containing velocity data.
+        force:                  A numpy array shaped (M, N, 3) containing force data.
+        stress:                 A numpy array shaped (M, N, 6) containing stress data.
+        data:                   A dictionary containing data from the .dat file.
+        thermodynamic_data:     A pandas dataframe containing thermodynamic data.
+    """
+
     trj, local2global, global2local=readTRJFile(trj_filename, True, None)
     vel=readTRJFile(vel_filename, True, None)[0]
     force=readTRJFile(force_filename, True, None)[0]
