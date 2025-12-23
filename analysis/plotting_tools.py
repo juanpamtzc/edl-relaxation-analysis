c6516634a954a1277fe4f61c0611cf51a36690a5
JPMC <jp.martinez.cordeiro@utexas.edu>
2025-12-23 17:15:33 -0600

    Added function headers and type hinting

--- analysis/plotting_tools.py
+++ analysis/plotting_tools.py
@@ -1,8 +1,22 @@
 import matplotlib.pyplot as plt
+import numpy as np
+from typing import Optional
 
-# This function plots a give time series
 # FUTURE WORK: add support for multiple series on the same plot
-def plot_time_series(time, data, title, xlabel, ylabel, output_file, xlim=None, ylim=None):
+def plot_time_series(time: np.array, data: np.array, title: str, xlabel: str, ylabel: str, output_file: str, xlim: Optional[tuple] = None, ylim: Optional[tuple] = None):
+    """
+    Plots a time series data.
+
+    Args:
+        time:                   A numpy array containing time values.
+        data:                   A numpy array containing data values corresponding to the time values.
+        title:                  The title of the plot.
+        xlabel:                 The label for the x-axis.
+        ylabel:                 The label for the y-axis.
+        output_file:            The filename where the plot will be saved.
+        xlim (Optional[tuple]): The limits for the x-axis (min, max). Default is None.
+        ylim (Optional[tuple]): The limits for the y-axis (min, max). Default is None.
+    """
 
     plt.figure(figsize=(10, 6))
     plt.plot(time, data)
@@ -19,8 +33,18 @@ def plot_time_series(time, data, title, xlabel, ylabel, output_file, xlim=None, 
     plt.savefig(output_file)
     plt.close()
 
-# This function plots a distribution function (histogram)
-def plot_distribution_function(data, bins, title, xlabel, ylabel, output_file):
+def plot_distribution_function(data: np.array, bins: np.array, title: str, xlabel: str, ylabel: str, output_file: str):
+    """
+    Plots a distribution function as a histogram.
+
+    Args:
+        data:                   A numpy array containing data values.
+        bins:                   A numpy array defining the bin edges for the histogram.
+        title:                  The title of the plot.
+        xlabel:                 The label for the x-axis.   
+        ylabel:                 The label for the y-axis.
+        output_file:            The filename where the plot will be saved.
+    """
 
     plt.figure(figsize=(10, 6))
     plt.hist(data, bins=bins, density=True, alpha=0.7, color='blue', edgecolor='black')
@@ -31,8 +55,20 @@ def plot_distribution_function(data, bins, title, xlabel, ylabel, output_file):
     plt.savefig(output_file)
     plt.close()
 
-# This function plots a 2D heatmap
-def plot_2d_heatmap(data, xlim, ylim, title, xlabel, ylabel, colormap_label, output_file):
+def plot_2d_heatmap(data: np.array, xlim: tuple, ylim: tuple, title: str, xlabel: str, ylabel: str, colormap_label: str, output_file: str):
+    """
+    Plots a 2d heatmap.
+
+    Args:
+        data:                   A 2D numpy array containing the heatmap data.
+        xlim:                   A tuple defining the limits for the x-axis (min, max).
+        ylim:                   A tuple defining the limits for the y-axis (min, max).
+        title:                  The title of the plot.
+        xlabel:                 The label for the x-axis.
+        ylabel:                 The label for the y-axis.
+        colormap_label:         The label for the colorbar.
+        output_file:            The filename where the plot will be saved.
+    """
     
     plt.figure(figsize=(10, 6))
     plt.imshow(data, aspect='auto', extent=[xlim[0], xlim[1], ylim[0], ylim[1]], origin='lower', cmap='viridis')
