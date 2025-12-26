616c5e6e05901a803758343ffb4f5073d12888f0
JPMC <jp.martinez.cordeiro@utexas.edu>
2025-12-26 13:10:22 -0600

    added tools to save and load timeseries data to/from txt files

--- analysis/data_processing_tools.py
+++ analysis/data_processing_tools.py
@@ -1,5 +1,6 @@
 from analysis.reading_tools import readTRJFile, readDatFile, readTRJFile_stresses, readLAMMPSThermodynamicFile
 from typing import Optional
+import numpy as np
 
 def process_filename(trj_filename: str, vel_filename: str, force_filename: str, stress_filename: str, dat_filename: str, thermo_filename: str, thermo_process_ID: Optional[int] = 1) -> tuple:
     """
@@ -32,3 +33,30 @@ def process_filename(trj_filename: str, vel_filename: str, force_filename: str, 
     data=readDatFile(dat_filename)
     thermodynamic_data=readLAMMPSThermodynamicFile(thermo_filename, thermo_process_ID)
     return trj, global2local, local2global, vel, force, stress, data, thermodynamic_data
+
+def save_timeseries_as_txt(time: np.array, data: np.array, output_filename: str):
+    """
+    Saves the provided time series data to a text file.
+
+    Args:
+        time:               A numpy array containing time values.
+        data:               A numpy array containing corresponding data values.
+        output_filename:    The filename for the output text file.
+    """
+    combined_data = np.column_stack((time, data))
+    np.savetxt(output_filename, combined_data, header="Time\tData")
+
+def read_timeseries_from_txt(filename: str) -> tuple:
+    """
+    Reads time series data from a text file.
+
+    Args:
+        filename:   The filename of the text file containing time series data.
+    
+    Returns:
+        A tuple containing two numpy arrays: time values and corresponding data values.
+    """
+    data = np.loadtxt(filename, skiprows=1)
+    time = data[:, 0]
+    values = data[:, 1:]
+    return time, values
